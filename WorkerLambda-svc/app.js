6a79bab28377e9ab0f7ec5f1293432f7269265d0
syedsaadahmed1 <ahmedfn@amazon.com>
2022-03-21 11:56:47 -0400

    Updated headers

--- WorkerLambda-svc/app.js
+++ WorkerLambda-svc/app.js
@@ -2,14 +2,13 @@
  *  SPDX-License-Identifier: MIT-0
  */
 
-// 1. Receive event from API Gateway HTTP API.
+// 1. Receive event from SQS Queue.
 // 2. Log event and context object to CloudWatch Logs.
-// 3. Send SNS message and log results to CloudWatch Logs. 
-// 3. Return a response with event information and SNS message to the caller.
+// 3. Log SQS message to CloudWatch Logs. 
+// 3. Return a null response.
 
 const AWS = require('aws-sdk')
 AWS.config.region = process.env.AWS_REGION 
-const sns = new AWS.SNS({apiVersion: '2012-11-05'})
 
 exports.handler = async (event, context) => {
   try {
